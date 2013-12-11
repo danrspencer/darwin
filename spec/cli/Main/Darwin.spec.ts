ce9ab5f1f6d9b0f490dbd383202b820332636fd3
Dan Spencer <danrspen@gmail.com>
2013-12-11 21:29:53 +0000

    Begun work on command line too

--- spec/cli/Main/Darwin.spec.ts
+++ spec/cli/Main/Darwin.spec.ts
@@ -1,9 +1,41 @@
-/// <reference path="../../../bower_components/DefinitelyTyped/jasmine/jasmine.d.ts" />
+/// <reference path="../ref.d.ts" />
+import jasmine_tss = require('../jasmine_tss'); var setSpy = jasmine_tss.setSpy, spyOf = jasmine_tss.spyOf;
+
+import Darwin = require('../../../lib/cli/Main/Darwin');
+
+import fs = require('fs');
+import promptly = require('promptly');
+
 
 describe('Darwin', () => {
 
-  it('creates a new test file', () => {
+  var promptlySpy: typeof promptly;
+  var fsSpy: typeof fs;
 
+  var darwin: Darwin;
+
+  beforeEach(() => {
+    promptlySpy = jasmine.createSpyObj<typeof promptly>('promptlySpy', ['prompt']);
+    fsSpy = jasmine.createSpyObj<typeof fs>('fsSpy', ['mkdirSync']);
+
+    darwin = new Darwin(fsSpy, promptlySpy);
+  });
+
+  it('delegates to promptly to prompt the user to enter the test description', () => {
+    darwin.init();
+
+    expect(promptlySpy.prompt).toHaveBeenCalledWith('Enter a test description: ', jasmine.any(Function));
+  });
+
+  it('delegates creates a directory named after the test', () => {
+
+    setSpy(promptlySpy.prompt).toCallFake((value: string, callback: Function) => {
+      callback(null, 'test desc');
+    });
+
+    darwin.init();
+
+    expect(fsSpy.mkdirSync).toHaveBeenCalledWith('test desc');
   });
 
 });
