d4800f6c2e1d0fbc7869df4dffaf4d55cf796273
Dan Spencer <danrspen@gmail.com>
2013-12-20 15:50:58 +0000

    Added screenshot capture class

---
+++ spec/browser/Screenshot/Capture.spec.ts
@@ -0,0 +1,7 @@
+import jasmine_tss = require('../../jasmine_tss'); var setSpy = jasmine_tss.setSpy, spyOf = jasmine_tss.spyOf;
+
+describe('Capture', ()=> {
+
+    
+
+});
---
+++ src/browser/Screenshot/Capture.ts
@@ -0,0 +1,7 @@
+
+class Capture {
+
+  public start() {
+
+  }
+}
